0f00e906d7e136c96072f4aa2359f1cb5905cad5
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-08-14 13:21:38 -0300

    added new routes project

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
+import ForgotPass from "../pages/ForgotPass/ForgotPass";
+import CodeVerifyUser from "../pages/CodeVerifyUser/CodeVerifyUser";
 
 const isAuthenticated = false;
 const PrivateRoute = ({ component: Component, ...rest }) => (
@@ -21,7 +23,9 @@ const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Login} />
-      <Route path="/signup" component={Register} />
+      <Route path="/cadastro" name="signup" component={Register} />
+      <Route path="/esqueci-minha-senha" component={ForgotPass} />
+      <Route path="/confirmar-identidade" component={CodeVerifyUser} />
       <PrivateRoute path="/app" component={() => <h1>App</h1>} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
